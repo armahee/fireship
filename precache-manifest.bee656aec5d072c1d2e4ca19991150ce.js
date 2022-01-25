79ad5f0e07846220c27ad674c5b0757fee0f6777
armahee <arifrayhan911@gmail.com>
2022-01-25 20:52:13 +0600

    first

---
+++ precache-manifest.bee656aec5d072c1d2e4ca19991150ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144c3f1fc096cd04bcfa138eb4072fb3",
+    "url": "/fireship/index.html"
+  },
+  {
+    "revision": "8508026ca891854ae229",
+    "url": "/fireship/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "faf2f738aa52b6c006df",
+    "url": "/fireship/static/js/2.0e3cf04b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/fireship/static/js/2.0e3cf04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8508026ca891854ae229",
+    "url": "/fireship/static/js/main.c9180ddd.chunk.js"
+  },
+  {
+    "revision": "2a4c5fb3fccde221d4e7",
+    "url": "/fireship/static/js/runtime-main.541bba21.js"
+  }
+]);
